6a91afae837425ad197fe14da974d1d0ccd014e6
Mahendra Fathir Amrulloh <mahendrafathir9@gmail.com>
2026-05-16 20:17:47 +0700

    menambahkan file laporan

--- kategori_produk.php
+++ kategori_produk.php
@@ -176,12 +176,11 @@
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Data Tables</h1>
+      <h1>Kategori Produk</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-          <li class="breadcrumb-item">Tables</li>
-          <li class="breadcrumb-item active">Data</li>
+          <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+          <li class="breadcrumb-item active">Kategori Produk</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
